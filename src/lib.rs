01067597e9f392aa29f9114e1f3a9cc32055f827
贺俊浩 <nougat_hx@qq.com>
2024-05-28 17:14:13 +0800

    init

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,9 @@
-use std::{error::Error, fs};
+use std::{env, error::Error, fs};
 
 pub struct Config {
     query: String,
     file_path: String,
+    ignore_case: bool,
 }
 impl Config {
     pub fn build(args: &[String]) -> Result<Self, &'static str> {
@@ -12,12 +13,22 @@ impl Config {
         }
         let query = args[1].clone();
         let file_path = args[2].clone();
-        Ok(Self { query, file_path })
+        let ignore_case = env::var("IGNORE_CASE ").is_ok();
+        Ok(Self {
+            query,
+            file_path,
+            ignore_case,
+        })
     }
 }
 pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
     let file_txt = fs::read_to_string(config.file_path)?;
-    for line in search(&config.query, &file_txt) {
+    let reuslt = if config.ignore_case {
+        case_search(&config.query, &file_txt)
+    } else {
+        search(&config.query, &file_txt)
+    };
+    for line in reuslt {
         println!("{line}")
     }
     Ok(())
@@ -33,6 +44,16 @@ pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
     result
 }
 
+pub fn case_search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    let query = query.to_lowercase();
+    let mut results = Vec::new();
+    for line in contents.lines() {
+        if line.to_lowercase().contains(&query) {
+            results.push(line.trim());
+        }
+    }
+    results
+}
 #[cfg(test)]
 mod test {
     use super::*;
@@ -44,5 +65,17 @@ mod test {
         safe, fast, productive.
         Pick three.";
         assert_eq!(vec!["safe, fast, productive."], search(query, contents));
+    }
+    #[test]
+    fn case_search_test() {
+        let query = "saFe";
+        let contents = "\
+      Rust:
+      safe, fast, productive.
+      Pick three.";
+        assert_eq!(
+            vec!["safe, fast, productive."],
+            case_search(query, contents)
+        );
     }
 }
